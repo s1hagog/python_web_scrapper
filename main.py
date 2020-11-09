c794cc4d7e7fea20f8dee74cc0de6919915f5119
s1hagog <moshakalex@gmail.com>
2020-11-08 17:47:21 -0800

    web scrapper started

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+URL = 'https://www.monster.ca/jobs/search/?q=Marketing-Co__2Dordinator&where=Vancouver__2C-British-Columbia'
+page = requests.get(URL)
+
+# f = open('demo.txt', 'w')
+# f.write(page)
+# f.close()
